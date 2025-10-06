fca729985db1b1c2eab854d21113d7984b46f61c
lharikrishnanml-sketch <lharikrishnanml@gmail.com>
2025-10-06 15:56:59 -0400

    Add files via upload

---
+++ faircopy.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+from multipleFunctions import multipleFunctions
+mf=multipleFunctions()
+
+
+# In[5]:
+
+
+mf.perm_triangle()
+mf.OddEven()
+
+
+# In[6]:
+
+
+mf.subfields()
+
+
+# In[12]:
+
+
+mf.Eligible()
+
+
+# In[13]:
+
+
+mf.percentage()
+
+
+# In[14]:
+
+
+mf.Areatriangle()
+
+
+# In[15]:
+
+
+mf.perm_triangle()
+
+
+# In[ ]:
+
+
+
+
